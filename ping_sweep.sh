3920813d96ec7143b78cadfbd46b97fc308c3dcf
Cyber Range Student <student@ip-10-1-153-101.ec2.internal>
2022-09-18 22:33:24 +0000

    Fix bad repo

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -3,14 +3,6 @@
 echo "Enter the Subnet:"
 read Subnet
 
-if [ "$Subnet" == "" ] 
-then
-echo "Enter the Subnet:"
-echo "Syntax Example = ./ping sweep.sh 10.1.154"
-else
-
 for IP in $(seq 1 254); do
-	
-	ping -c 1 $Subnet.$IP | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> discoveredIPs.txt &
+	ping -c 1 $Subnet.$IP | grep "64 bytes" | cut -d " " -f 4 &
 done
-fi
