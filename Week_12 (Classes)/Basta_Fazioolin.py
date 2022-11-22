19d1bafd1d3471d7eae4210bd07083fb99fe25d5
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-22 18:57:40 -0500

    Update Basta_Fazioolin.py

--- Week_12 (Classes)/Basta_Fazioolin.py
+++ Week_12 (Classes)/Basta_Fazioolin.py
@@ -1,77 +1,75 @@
-class Franchise:
-    pass
-    def __init__(self, address, menus):
+class Franchise
+  def __init(self, address, menus):
         self.address = address
         self.menus = menus
-        
-    def __repr__(self):
-        return self.address
-        
-    def available_menus(self, time):
-        available_menus = []
-        for menu in self.menus:
-            if time >= menu.start_time and time <= menu.end_time:
-                available_menus.append(menu)
-        return available_menus
 
 class Menu:
-    pass
     def __init__(self, name, items, start_time, end_time):
         self.name = name
         self.items = items
         self.start_time = start_time
         self.end_time = end_time
-    
-    def __repr__(self):
-        return f'{self.name} is available from {self.start_time} to {self.end_time}.'
-    
-    def calculate_bill(self, purchased_items):
-        bill = 0
-        for purchased_item in purchased_items:
-            if purchased_item in self.items:
-                bill += self.items[purchased_item]
-            else:
-                print('At least one item is not available on the menu.')
-                return None
-        return bill
 
+    def __repr__(self):
+        return self.name + " menu is available from " + str(self.start_time) + " to " + str(self.end_time)
+
+    def calculate_bill(self, purchased_items):
+      bill = 0
+      for purchased_item in purchased_items:
+        if purchased_item in self.items:
+          bill += self.items[purchased_item]
+      return bill
+    
 # Brunch Menu
-brunch_menu = {
-    'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50,
-    'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50,
-    'orange juice': 3.50
-    }
-brunch = Menu('Brunch', brunch_menu, 1100, 1600)
+brunch_items = {
+  'pancakes': 7.50,
+  'waffles': 9.00,
+  'burger': 11.00,
+  'home fries': 4.50,
+  'coffee': 1.50,
+  'espresso': 3.00,
+  'tea': 1.00,
+  'mimosa': 10.50,
+  'orange juice': 3.50
+}
+brunch = Menu('Brunch', brunch_items, 1100, 1600)
 
 # Early Bird Menu
-early_bird_menu = {
-    'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00,
-    'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50,
-    'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00
-    }
-early_bird = Menu('Early Bird', early_bird_menu, 1500, 1800)
+early_bird_items = {
+  'salumeria plate': 8.00,
+  'salad and breadsticks (serves 2, no refills)': 14.00,
+  'pizza with quattro formaggi': 9.00,
+  'duck ragu': 17.50,
+  'mushroom ravioli (vegan)': 13.50,
+  'coffee': 1.50,
+  'espresso': 3.00
+}
+early_bird_menu = Menu('Early Bird', early_bird_items, 1500, 1800)
+
+print(early_bird_menu.calculate_bill(['salumeria    plate', 'mushroom ravioli (vegan)', 'coffee']))
 
 # Dinner Menu
-dinner_menu = {
-    'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00,
-    'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50,
-    'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00
-    }
-dinner = Menu('Dinner', dinner_menu, 1700, 2300)
+dinner_items = {
+  'crostini with eggplant caponata': 13.00,
+  'ceaser salad': 16.00,
+  'pizza with quattro formaggi': 11.00,
+  'duck ragu': 19.50,
+  'mushroom ravioli (vegan)': 13.50,
+  'coffee': 2.00,
+  'espresso': 3.00
+}
+dinner = Menu('Dinner', dinner_items, 1700, 2300)
 
 # Kids Menu
-kids_menu = {
-    'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00,
-    'apple juice': 3.00
-    }
-kids = Menu('Kids', kids_menu, 1100, 2100)
+kids_items = {
+  'chicken nuggets': 6.50,
+  'fusilli with wild mushrooms': 12.00,
+  'apple juice': 3.00
+}
+kids = Menu('Kids', kids_items, 1100, 2100)
 
 # All Menu OBJECTS
-menus = [brunch, early_bird, dinner, kids]
+menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
 
-# 12
-flagship_store = Franchise('1232 West End Road', menus)
-new_installment = Franchise('12 East Mulberry Street', menus)
-
-
-print(flagship_store.available_menus(1200))
+flagship_store = Franchise('1234 West End Rd', menus)
+new_installments = Franchise('12 East Mulberry Street', menus)
