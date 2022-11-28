491ce07c00f118a8a280118b5a14b1b66932645d
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-27 23:37:13 -0500

    Update CommentsRequest.py

--- Week_13 ( Reconnaissance | Scanning)/Remote Comments/CommentsRequest.py
+++ Week_13 ( Reconnaissance | Scanning)/Remote Comments/CommentsRequest.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+import requests
+
+cookies = {'PHPSESSID':'<YOUR COOKIE VALUE>', 'security':'low'}
+
+url='http://dvwa.example.com/vulnerabilities/xss_s/' form_input = open("/home/student/Desktop/scripts/keyboard_Input.txt") form_send = form_input.read()
+form_data = {'txtName':'Test IT', 'mtxMessage':f"'{form_send}'", 'btnSign':'Sign+Guestbook'}
+r = requests.post(url, cookies=cookies, data=form_data)
